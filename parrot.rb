01a4687d8a41acbf5ad592e895ac0f10d691c9df
Valentina E <tiyahelfrih@gmail.com>
2020-07-04 11:26:27 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot
+  puts "Squawk!"
+end
+endparrot # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  puts best_parrot = "parrot"
+  return "Squawk!"
+end
